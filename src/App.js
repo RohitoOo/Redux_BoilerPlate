776490ab40c89cbacaf6ae329237edb966ea42a4
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-29 20:22:20 +0530

    Install Redux, react-redux & redux-thunk. Added Store to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import Posts from './components/post'
 import PostForm from './components/postForm'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+const store = createStore( () => {
+  [] , applyMiddleware()
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm></PostForm>
-        <Posts></Posts>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm></PostForm>
+          <Posts></Posts>
+        </div>
+      </Provider>
     );
   }
 }
