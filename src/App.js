a9320a56a7565d78433652c6e42a6b5b30eff60d
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-29 15:35:50 +0530

    Added PostForm Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/post'
+import PostForm from './components/postForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Posts></Posts>
+        <PostForm></PostForm>
+        <Posts></Posts>
       </div>
     );
   }
