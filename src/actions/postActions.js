3201edcb33075673ddee9263436ca2c6c754c254
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-30 17:39:43 +0530

    Added Second Action -- New_Post

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -14,3 +14,25 @@ console.log('fetching');
 };
 
 //Data coming in to the Reducer ( Payload )
+
+
+export const createPost = (postData) => dispatch => {
+  console.log('Action Called')
+
+  fetch('https://jsonplaceholder.typicode.com/posts' , {
+    method : 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body : JSON.stringify(postData)
+  })
+  .then(res => res.json())
+  .then(post => dispatch({
+    type: NEW_POST,
+    payload: post
+   })
+ );
+
+};
+
+//Data coming in to the Reducer ( Payload )
--- src/components/postForm.js
+++ src/components/postForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { createPost } from '../actions/postActions'
 
 class PostForm extends Component {
 
@@ -27,17 +30,11 @@ onSubmit(e){
     body : this.state.body
   }
 
-  fetch('https://jsonplaceholder.typicode.com/posts' , {
-    method : 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body : JSON.stringify(post)
-  })
-  .then(res => res.json())
-  .then(data => console.log(data))
 
 
+// Call Action
+
+this.props.createPost(post)
 
 }
 
@@ -70,4 +67,8 @@ onSubmit(e){
 
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null , {createPost})(PostForm);
